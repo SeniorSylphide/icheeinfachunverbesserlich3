7f2805d9494f0a65ad46e351439a832d0d572f1a
SeniorSylphide <30345912+SeniorSylphide@users.noreply.github.com>
2017-07-21 15:36:37 +0200

    Create README.m

---
+++ README.m
@@ -0,0 +1,44 @@
+~~~ ICH EINFACH UNVERBESSERLICH 3 JETZT KOSTENLOS ANSEHEN ~~~ DEN NEUEN MINIONS FILM KOSTENLOS STREAMEN ~~~
+
+
+***************************************************************************
+*   HIER KLICKEN, um den FILM GRATIS ONLINE ANZUSEHEN: http://j.gs/9DR4   *
+***************************************************************************   
+
+Wenn du auf diesen Link klickst kommst du auf eine Filme-Seite, auf der du den neuen Film mit den Minions "Ich einfach unverbesserlich 3" 
+ansehen kannst. Du kannst den Film komplett kostenlos streamen.
+Alternativ kannst du die Datei auch herunterladen.
+
+Viel Spaß beim kostenlosten anschauen von "Ich einfach unverbesserlich 3"!
+
+
+
+~~~ Handlung ~~~
+
+Gru und Lucy versuchen Balthazar Bratt - einen ehemaligen und scheinbar noch in den 80ern hängen gebliebenen Kinderstar einer 
+damals erfolgreichen Fernsehsendung - aufzuhalten einen riesigen rosanen Diamanten zu stehlen. 
+Balthazar hasst die Welt und vor allem Hollywood dafür, dass seine Serie „Bösewicht Bratt“ abgesetzt wurde. 
+Daraufhin hat er sich einen finsteren Plan ausgedacht, der ihm am Ende die Weltherrschaft sichern soll, 
+damit er nie wieder von anderen ausgelacht wird. 
+
+Gru und Lucy werden jedoch, nachdem sie Balthazar den Diebstahl verhindern, ihn aber nicht festnehmen haben können, 
+von der neuen Leiterin der AVL (Anti-Verbrecher-Liga) Valerie Da Vinci gekündigt. 
+Balthazar war es danach in Verkleidung gelungen, den riesigen rosa Diamanten zu stehlen. 
+Zu diesem Problem kommt hinzu, dass Grus Zwillingsbruder Dru, von dem seine Mutter die Existenz verheimlichen wollte, 
+zusammenfinden und es zu geschwisterlichen Rivalitäten kommt. 
+Im Gegensatz zu Grus alten Zeiten und ihren Vorfahren ist Dru ein miserabler Schurke, dem es aber nicht an Geld mangelt. 
+Nach einem Kennenlerntag kann Gru sein Angebot mit ihm zusammen einen letzten Coup zu landen dennoch nicht abschlagen. 
+Gemeinsam wollen sie Balthazar Bratt den Diamanten wieder entwenden. 
+
+Gru verheimlicht seinem Bruder jedoch, dass er den Diamanten benutzen will, um seinen und Lucys Job bei der AVL zurückzukriegen. 
+Sie schaffen es in Balthazars Versteck einzudringen und mithilfe von Lucy flüchten sie mit dem Diamanten zurück in Drus Anwesen. 
+Erschrocken, dass Dru von seinem Bruder angelogen und betrogen wurde, zerstreiten sich die beiden. 
+Währenddessen schnappt sich Balthazar verkleidet als Lucy den Diamanten wieder und entführt Grus Töchter. 
+Um die Kinder zu befreien und Balthazar daran zu hindern, Hollywood zu zerstören und ins Weltall zu befördern, 
+arbeiten die Brüder und Lucy noch einmal zusammen. 
+
+Gemeinsam schaffen sie es Balthazar das Handwerk zu legen und die Kinder und die Stadt zu retten. 
+Gru und Dru vertragen sich wieder. Anschließend werden Gru und Lucy für die Festnahme von Bratt wieder bei der AVL angestellt. 
+Dru wohnt nun bei Gru und zusammen mit den Minions, die das Schurkendasein vermissten, 
+ist er als Schurke tätig, um die Familientradition aufrecht zu erhalten. 
+Als er eines Nachts auf schurkische Mission geht, gibt Gru ihm einen Vorsprung von 5 Minuten, bevor er seinen Bruder jagt.
